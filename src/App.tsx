397d5ce0abf5ed81ea07790425a563f6d305bd84
jonnelrodjiptolosa <jonnelrodji.tolosa@hcl.com>
2021-11-24 17:08:41 +0800

    Fix onChange behavior and state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import OutputBox from './components/OutputBox';
 import './App.css';
 
 const App = () => {
+  const [output, setOutput] = useState<string>('');
+  const [input, setInput] = useState<string>('')
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    // Don't let user type alphabet characters
+    if (value.match("[a-zA-Z]+")) {
+      return;
+    }
+    setInput(value);
+    const splitValues = value.split(',');
+    const doubledValues = splitValues.map((input) => {
+      return Number(input) * 2;
+    });
+    setOutput(String(doubledValues));
+  };
+
   return (
     <div className="App">
       <h1>MediCords Frontend Challenge</h1>
@@ -9,13 +27,9 @@ const App = () => {
         <div className="box-container">
           <h3>Input</h3>
           <p>Array</p>
-          <input type="text" />
+          <input type="text" value={input} onChange={(e: React.ChangeEvent<HTMLInputElement>) => { return handleInputChange(e); }} />
         </div>
-        <div className="box-container">
-          <h3>Output</h3>
-          <p>Double</p>
-          <input type="text" />
-        </div>
+        <OutputBox output={output} />
       </div>
     </div>
   );
